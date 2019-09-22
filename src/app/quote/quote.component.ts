a174a5a7f9e891d0d175086844466c5d1bd2d559
eric <eric.kingoo2014@gmail.com>
2019-09-22 16:12:37 +0300

    updated the qoute and quote-detail component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    new Quote(1, 'Trevor Noah', 'Don’t worry about the world coming to an end today coz guess what, It’s already tomorrow in Australia.'),
+    new Quote(2,'Amy Poehler','If People are talking behind your back, jus fart'),
+    new Quote(3,'Vin Diesel','Dont Break someones heart,they only have one, break their bones,they have 206 of them'),
+    new Quote(4,'Chris Rock','Accept who you are. Unless you are a serial killer'),
+    new Quote(5,'Trevor Noah','Does America really need to the best at everything? You already dominate the world in economics, military power and obesity.'),
+    new Quote(6,'Kevin Hart','I DON’T have EX’s! I have Y’s. Like ‘Y the hell did I date you?!'),
+  ];
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
 
   constructor() { }
 
